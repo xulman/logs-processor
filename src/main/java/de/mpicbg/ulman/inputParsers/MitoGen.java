881f1c09ad80daab487a275033e23fa2d5938dbf
Vladimír Ulman <ulman@mpi-cbg.de>
2017-11-28 10:58:47 +0100

    CHG: MitoGen parser reads the whole log without any log file modification

--- src/main/java/de/mpicbg/ulman/inputParsers/MitoGen.java
+++ src/main/java/de/mpicbg/ulman/inputParsers/MitoGen.java
@@ -23,6 +23,14 @@ public class MitoGen extends AbstractParser
 	///the handle to the log file
 	final BufferedReader reader;
 
+	/**
+	 * what section of the MitoGen log are we reading now:
+	 * 0 - initial part, line by line, assigned to "Thread 1"
+	 * 1 - middle part, by pairs of lines, assigned to parsed threads
+	 * 2 - closing part, line by line, assigned to "Thread 1"
+	 */
+	int readingPhase = 0;
+
 	///constructor
 	public
 	MitoGen(final File logFile)
@@ -30,16 +38,6 @@ public class MitoGen extends AbstractParser
 	{
 		//open the log file
 		reader = Files.newBufferedReader(logFile.toPath());
-
-		//reads until the intro part is over
-		String line = reader.readLine();
-		while (  line != null
-		      && line.startsWith("scheduler.cpp::Run_st(): Starting") == false)
-			line = reader.readLine();
-
-		if (line == null)
-			throw new IOException("Incorrect log format during initialization.");
-			//isThereNext = false; //alternatively, just block from other reading
 	}
 
 	protected void finalize()
@@ -58,39 +56,97 @@ public class MitoGen extends AbstractParser
 	void readNextXYMsg()
 	{
 		try {
-			//get always two lines from the file
-			final String lineH = reader.readLine();
-			final String lineM = reader.readLine();
-			if (lineH == null || lineM == null)
-				throw new IOException("problem reading pair of lines");
-
-			//have we reached end of file?
-			if (! reader.ready())
-			{
-				reader.close();
-				isThereNext = false;
-			}
-
 			//prepare message container
 			currentEvent.msg.clear();
 
-			//sample log line:
-			//Thread 1 at 00:05:04:248, Cell ID: 3,
+			//helper whole-log-line-holder/variable for the switch
+			String line = null;
+			switch (readingPhase)
+			{
+			case 0:
+				currentEvent.x = "Thread 1";
+				currentEvent.y = counter++; counter+=2; //for the title and for an empty separating line
+				currentEvent.msg.add("00:00, initialization"); //title of the msg
 
-			//the message source -- thread ID
-			final int atPos = lineH.indexOf(" at");
-			currentEvent.x = lineH.substring(0,atPos);
+				//reads until the intro part is over
+				line = reader.readLine();
+				while (  line != null
+						&& line.startsWith("scheduler.cpp::Run_st(): Starting") == false)
+				{
+					currentEvent.msg.add(line); //body of the msg
+					line = reader.readLine();
+				}
 
-			//could extract time here....
+				if (line == null)
+					throw new IOException("Incorrect log format during initialization.");
+					//isThereNext = false; //alternatively, just block from other reading
+				currentEvent.msg.add(line); //body of the msg (the line that startsWith() == true)
 
-			//line number as 'y'
-			currentEvent.y = counter;
-			counter += 2;
-			//NB: were utilizing Event.title here, hence consider 2 lines per event
+				readingPhase = 1;
+				break;
 
-			//the message title and body
-			currentEvent.msg.add(lineH.substring(atPos+4));
-			currentEvent.msg.add(lineM);
+			case 1:
+				//process always two lines from the file
+				//get the first line
+				line = reader.readLine();
+				if (line == null)
+					throw new IOException("problem reading first in the pair of lines");
+
+				//sample "line" log line:
+				//Thread 1 at 00:05:04:248, Cell ID: 3,
+
+				final int atPos = line.indexOf(" at");
+				if (atPos > 0)
+				{
+					//likely still in the middle section of the log,
+					//continue processing it and read the second line later too
+
+					//the message source -- thread ID
+					currentEvent.x = line.substring(0,atPos);
+
+					//could extract time here....
+
+					//line number as 'y'
+					currentEvent.y = counter;
+					counter += 2;
+					//NB: were utilizing Event.title here, hence consider 2 lines per event
+
+					//the message title
+					currentEvent.msg.add(line.substring(atPos+4));
+
+					//second line -- the message body
+					line = reader.readLine();
+					if (line == null)
+						throw new IOException("problem reading second in the pair of lines");
+					currentEvent.msg.add(line);
+					break;
+				}
+
+				//if we got here, we likely have just reached first line of the last log section
+				readingPhase = 2;
+				currentEvent.x = "Thread 1";
+				currentEvent.y = counter+1; //for an empty separating line
+				currentEvent.msg.add("99:99, closing"); //title of the msg
+				currentEvent.msg.add(line);             //first line of the body of the msg
+
+			case 2:
+				//read until the end of file?
+				while (reader.ready())
+				{
+					line = reader.readLine();
+					if (line == null)
+						throw new IOException("Incorrect log format during closing.");
+					currentEvent.msg.add(line); //body of the msg
+				}
+
+				isThereNext = false;
+				reader.close();
+
+				readingPhase = 99; //make it jump to the empty default, just in case...
+				break;
+
+			default:
+			}
 		}
 		catch (IOException e) {
 			 System.err.format("IOException: %s%n", e);
@@ -98,5 +154,5 @@ public class MitoGen extends AbstractParser
 		}
 	}
 
-	private long counter = 1;
+	private long counter = 0;
 }
